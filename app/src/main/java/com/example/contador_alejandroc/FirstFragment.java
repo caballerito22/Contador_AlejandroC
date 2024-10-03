86a75329aeab3fa0afc227905dd3921db12e5334
alecabnun <avestruz2265@gmail.com>
2024-10-03 09:22:24 +0200

    caca

--- app/src/main/java/com/example/contador_alejandroc/FirstFragment.java
+++ app/src/main/java/com/example/contador_alejandroc/FirstFragment.java
@@ -24,59 +24,59 @@ public class FirstFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        View view = inflater.inflate(R.layout.fragment_first, container, false);
-
         binding = FragmentFirstBinding.inflate(inflater, container, false);
-        reset();
         return binding.getRoot();
-
     }
 
+    @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        vida1 =20;
-        vida2 = 20;
-        pocion1=0;
-        pocion2=0;
 
-        if (savedInstanceState != null){
+        if (savedInstanceState != null) {
             vida1 = savedInstanceState.getInt("vida1");
             pocion1 = savedInstanceState.getInt("pocion1");
+            vida2 = savedInstanceState.getInt("vida2");
+            pocion2 = savedInstanceState.getInt("pocion2");
+        } else {
+            reset();
         }
 
+        refresh();
+
+
         binding.PMasUno.setOnClickListener(v -> {
-            if (pocion1>=0) {
+            if (pocion1 >= 0) {
                 pocion1++;
                 refresh();
             }
         });
         binding.PMenosUno.setOnClickListener(v -> {
-            if (pocion1>=0) {
+            if (pocion1 >= 0) {
                 pocion1--;
                 refresh();
             }
         });
         binding.PMasDos.setOnClickListener(v -> {
-            if (pocion2>=0) {
+            if (pocion2 >= 0) {
                 pocion2++;
                 refresh();
             }
         });
         binding.PMenosDos.setOnClickListener(v -> {
-            if (pocion2>=0) {
+            if (pocion2 >= 0) {
                 pocion2--;
                 refresh();
             }
         });
         binding.masVidaArriba.setOnClickListener(v -> {
-            if (vida1>0) {
+            if (vida1 > 0) {
                 vida1++;
                 refresh();
             }
         });
         binding.menosVidaArriba.setOnClickListener(v -> {
-            if (vida1>0) {
+            if (vida1 > 0) {
                 vida1--;
                 refresh();
             }
@@ -88,48 +88,54 @@ public class FirstFragment extends Fragment {
             }
         });
         binding.menosVidaAbajo.setOnClickListener(v -> {
-            if (vida2>0) {
+            if (vida2 > 0) {
                 vida2--;
                 refresh();
             }
         });
         binding.flechaArriba.setOnClickListener(v -> {
-            if (vida2>0) {
+            if (vida2 > 0) {
                 vida2--;
                 vida1++;
                 refresh();
             }
         });
         binding.flechaAbajo.setOnClickListener(v -> {
-            if (vida1>0) {
-                vida2++;
+            if (vida1 > 0) {
                 vida1--;
+                vida2++;
                 refresh();
             }
-
         });
-
-
     }
+
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("vida1", vida1);
+        outState.putInt("pocion1", pocion1);
+        outState.putInt("vida2", vida2);
+        outState.putInt("pocion2", pocion2);
+    }
+
+
     private void refresh() {
-        binding.txp1a.setText(vida1 + "/" +pocion1);
+        binding.txp1a.setText(vida1 + "/" + pocion1);
         binding.txp1abajo.setText(vida2 + "/" + pocion2);
     }
 
 
     private void reset() {
-        pocion1 = 0;
-        pocion2 = 0;
         vida1 = 20;
         vida2 = 20;
+        pocion1 = 0;
+        pocion2 = 0;
     }
-
-
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
-
 }
